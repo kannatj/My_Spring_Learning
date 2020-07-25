400bcefcb04390ec3114bbd4cbfe54043452345e
KANNA <KANNA@DESKTOP-43DL0GS>
2020-07-25 22:53:25 +0530

    First commit

---
+++ springmvcorm/src/main/java/com/kanna/spring/springmvcorm/user/services/UserService.java
@@ -0,0 +1,14 @@
+package com.kanna.spring.springmvcorm.user.services;
+
+import java.util.List;
+
+import com.kanna.spring.springmvcorm.user.entity.User;
+
+public interface UserService {
+	
+	int save(User user);
+
+	List<User> getUsers();
+	
+	User getUser(Integer id);
+}
